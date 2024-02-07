86282c6075f1e1ce2085ed0919e8374e02f088ce
hnlim <email@>
2024-02-07 10:20:26 -0600

    JavaScript - function, optional parameter

--- topic5/functions.js
+++ topic5/functions.js
@@ -33,3 +33,26 @@ console.log(todayCelsius)
 todayCelsius = f_to_c(todayTemp,2)
 console.log(todayCelsius)
 
+
+// GPA function
+function isGPA(x) {
+    if (x>= 0 && x <= 4) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(isGPA(-3))
+console.log(isGPA('4.1'))
+console.log(isGPA(2.5))
+console.log(isGPA('4'))
+// console.log(isGPA(A)) // error
+
+
+// function that joins the name of a city and state using template string
+function cityCommaState(city, state) {
+    return `${city}, ${state.toUpperCase()}` // use back tick (`) and ${} for template string
+}
+
+console.log(cityCommaState('minneapolis', 'mn'))
