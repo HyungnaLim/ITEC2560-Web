2caa21f6dd23e0ba3659cbf5a189e67e79eee299
hnlim <email@>
2024-02-07 10:09:11 -0600

    JavaScript - variables (var, let, const), template string

--- topic5/hello.js
+++ topic5/hello.js
@@ -30,3 +30,8 @@ console.log('Today\'s temperature is ' + tempC.toFixed(2) + 'C') // rounding dec
 // using template string
 console.log(`Today's temperature is ${tempC.toFixed(2)}C
 which is equivalent to ${todayTemp}F`) // putting new line space is possible in JavaScript
+
+let className = 'Web Programming'
+let classCode = 2560
+let department = 'ITEC'
+console.log(`This class is ${department} ${classCode} ${className}`)
