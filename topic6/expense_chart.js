e32addc95489c68ee1a419e1b9b392c393d4b81c
hnlim <email@>
2024-02-21 17:18:08 -0600

    JavaScript libraries - ChartJS doughnut chart

---
+++ topic6/expense_chart.js
@@ -0,0 +1,94 @@
+/* Input elements */
+let expenseNameInput = document.querySelector('#expense-name')
+let expenseAmountInput = document.querySelector('#expense-amount')
+let addExpenseButton = document.querySelector('#add-expense')
+
+/* Get chart canvas and context  */
+let chartCanvas = document.querySelector('#expenses-doughnut-chart')
+let ctx = chartCanvas.getContext('2d')
+
+// Hide chart until data has been added
+chartCanvas.style.display = 'none'
+
+// create chart object
+let expensesChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        datasets: [
+            {
+                data: [],
+                backgroundColor: []
+            }
+        ],
+        labels: []
+    },
+    options: {}
+})
+
+
+// TODO (optional) replace with colors of your choice. The array can have as many or as few colors as you like
+let chartColors = [ 'tomato', 'orange', 'dodgerblue', 'mediumseagreen', 'slateblue', 'violet' ]
+
+
+function addExpenseToChart(name, amount) {
+
+    // Ensure chart is visible
+    chartCanvas.style.display = "block"
+
+    // add expense to chart
+    expensesChart.data.labels.push(name)
+    expensesChart.data.datasets[0].data.push(amount)
+
+    let colorCount = expensesChart.data.datasets[0].backgroundColor.length
+    let color = chartColors[colorCount % chartColors.length] // use modulo operator to always give valid number
+
+    expensesChart.data.datasets[0].backgroundColor.push(color)
+
+    expensesChart.update()
+
+}
+
+
+addExpenseButton.addEventListener('click', function() {
+
+    let errors = []
+
+    let expenseName = expenseNameInput.value
+    let expenseAmount = expenseAmountInput.value
+
+    // Validate both fields are filled in, and the amount is a positive number
+    if (expenseName.length == 0) {
+        errors.push('Enter a type of expense')
+    }
+
+    if (expenseAmount.length == 0 || expenseAmountInput < 0) {
+        errors.push('Enter a positive amount for the expense')
+    }
+
+    // If any errors, alert and return - do not procede to add to chart
+    if (errors.length > 0) {
+        alert( errors.join('\n') )
+        return
+    }
+
+    // call function to update chart
+    addExpenseToChart(expenseName, expenseAmount)
+
+    // Clear inputs, ready for next expense name and amount.
+    expenseNameInput.value = ''
+    expenseAmountInput.value = ''
+
+})
+
+
+// add event listener to click the Add Expense button when the enter key is pressed
+window.addEventListener('keyup', function () {
+    if (event.keyCode === 13) { // if the enter key is pressed
+        let inputElements = [expenseNameInput, expenseAmountInput, addExpenseButton]
+        if (inputElements.includes(document.activeElement) ) { // if the user is working with the form
+            addExpenseButton.click() // click the add expense button
+            expenseNameInput.focus() // make expense name input active
+        }
+    }
+    // If there's no active element in the page, nothing will happen when pressing enter
+})
