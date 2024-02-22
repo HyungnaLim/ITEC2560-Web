a940f841477c6c2e0556ca5cea5fabd8bf0b2335
hnlim <email@>
2024-02-21 18:51:07 -0600

    in class practice

---
+++ topic6/bus.js
@@ -0,0 +1,214 @@
+let bus_times = {
+    "stops": [
+    {
+        "stop_id": 17940,
+        "latitude": 44.973,
+        "longitude": -93.284325,
+        "description": "Hennepin Ave & Minneapolis College"
+    }
+],
+    "alerts": [],
+    "departures": [
+    {
+        "actual": false,
+        "trip_id": "24559037-DEC23-MVS-BUS-Weekday-01",
+        "stop_id": 17940,
+        "departure_text": "6:38",
+        "departure_time": 1708562280,
+        "description": "Downtown / Via Xerxes",
+        "route_id": "6",
+        "route_short_name": "6",
+        "direction_id": 0,
+        "direction_text": "NB",
+        "schedule_relationship": "NoData"
+    },
+    {
+        "actual": true,
+        "trip_id": "24559185-DEC23-MVS-BUS-Weekday-01",
+        "stop_id": 17940,
+        "departure_text": "13 Min",
+        "departure_time": 1708562943,
+        "description": "Johnson St / Silver Lk Vlg / Via Penn",
+        "route_id": "4",
+        "route_short_name": "4",
+        "direction_id": 0,
+        "direction_text": "NB",
+        "terminal": "B",
+        "schedule_relationship": "Scheduled"
+    },
+    {
+        "actual": true,
+        "trip_id": "24559076-DEC23-MVS-BUS-Weekday-01",
+        "stop_id": 17940,
+        "departure_text": "19 Min",
+        "departure_time": 1708563327,
+        "description": "27Av-Univ / Via France",
+        "route_id": "6",
+        "route_short_name": "6",
+        "direction_id": 0,
+        "direction_text": "NB",
+        "terminal": "U",
+        "schedule_relationship": "Scheduled"
+    },
+    {
+        "actual": false,
+        "trip_id": "24559189-DEC23-MVS-BUS-Weekday-01",
+        "stop_id": 17940,
+        "departure_text": "7:03",
+        "departure_time": 1708563780,
+        "description": "Johnson St / Silver Lk Vlg / Via Lyndale",
+        "route_id": "4",
+        "route_short_name": "4",
+        "direction_id": 0,
+        "direction_text": "NB",
+        "terminal": "B",
+        "schedule_relationship": "Scheduled"
+    },
+    {
+        "actual": false,
+        "trip_id": "24559041-DEC23-MVS-BUS-Weekday-01",
+        "stop_id": 17940,
+        "departure_text": "7:08",
+        "departure_time": 1708564080,
+        "description": "Downtown / Via Xerxes",
+        "route_id": "6",
+        "route_short_name": "6",
+        "direction_id": 0,
+        "direction_text": "NB",
+        "schedule_relationship": "Scheduled"
+    },
+    {
+        "actual": false,
+        "trip_id": "24559077-DEC23-MVS-BUS-Weekday-01",
+        "stop_id": 17940,
+        "departure_text": "7:23",
+        "departure_time": 1708564980,
+        "description": "27Av-Univ / Via France",
+        "route_id": "6",
+        "route_short_name": "6",
+        "direction_id": 0,
+        "direction_text": "NB",
+        "terminal": "U",
+        "schedule_relationship": "Scheduled"
+    },
+    {
+        "actual": false,
+        "trip_id": "24559191-DEC23-MVS-BUS-Weekday-01",
+        "stop_id": 17940,
+        "departure_text": "7:23",
+        "departure_time": 1708564980,
+        "description": "Johnson St / Silver Lk Vlg / Via Penn",
+        "route_id": "4",
+        "route_short_name": "4",
+        "direction_id": 0,
+        "direction_text": "NB",
+        "terminal": "B",
+        "schedule_relationship": "Scheduled"
+    },
+    {
+        "actual": false,
+        "trip_id": "24559042-DEC23-MVS-BUS-Weekday-01",
+        "stop_id": 17940,
+        "departure_text": "7:38",
+        "departure_time": 1708565880,
+        "description": "Downtown / Via Xerxes",
+        "route_id": "6",
+        "route_short_name": "6",
+        "direction_id": 0,
+        "direction_text": "NB",
+        "schedule_relationship": "Scheduled"
+    },
+    {
+        "actual": false,
+        "trip_id": "24559190-DEC23-MVS-BUS-Weekday-01",
+        "stop_id": 17940,
+        "departure_text": "7:43",
+        "departure_time": 1708566180,
+        "description": "Downtown / Via Lyndale",
+        "route_id": "4",
+        "route_short_name": "4",
+        "direction_id": 0,
+        "direction_text": "NB",
+        "schedule_relationship": "Scheduled"
+    },
+    {
+        "actual": false,
+        "trip_id": "24559078-DEC23-MVS-BUS-Weekday-01",
+        "stop_id": 17940,
+        "departure_text": "7:53",
+        "departure_time": 1708566780,
+        "description": "27Av-Univ / Via France",
+        "route_id": "6",
+        "route_short_name": "6",
+        "direction_id": 0,
+        "direction_text": "NB",
+        "terminal": "U",
+        "schedule_relationship": "Scheduled"
+    },
+    {
+        "actual": false,
+        "trip_id": "24559193-DEC23-MVS-BUS-Weekday-01",
+        "stop_id": 17940,
+        "departure_text": "8:03",
+        "departure_time": 1708567380,
+        "description": "Johnson St / Silver Lk Vlg / Via Penn",
+        "route_id": "4",
+        "route_short_name": "4",
+        "direction_id": 0,
+        "direction_text": "NB",
+        "terminal": "B",
+        "schedule_relationship": "Scheduled"
+    },
+    {
+        "actual": false,
+        "trip_id": "24559043-DEC23-MVS-BUS-Weekday-01",
+        "stop_id": 17940,
+        "departure_text": "8:08",
+        "departure_time": 1708567680,
+        "description": "Downtown / Via Xerxes",
+        "route_id": "6",
+        "route_short_name": "6",
+        "direction_id": 0,
+        "direction_text": "NB",
+        "schedule_relationship": "NoData"
+    },
+    {
+        "actual": false,
+        "trip_id": "24559075-DEC23-MVS-BUS-Weekday-01",
+        "stop_id": 17940,
+        "departure_text": "8:23",
+        "departure_time": 1708568580,
+        "description": "27Av-Univ / Via France",
+        "route_id": "6",
+        "route_short_name": "6",
+        "direction_id": 0,
+        "direction_text": "NB",
+        "terminal": "U",
+        "schedule_relationship": "NoData"
+    },
+    {
+        "actual": false,
+        "trip_id": "24559194-DEC23-MVS-BUS-Weekday-01",
+        "stop_id": 17940,
+        "departure_text": "8:30",
+        "departure_time": 1708569000,
+        "description": "Johnson St / Silver Lk Vlg / Via Lyndale",
+        "route_id": "4",
+        "route_short_name": "4",
+        "direction_id": 0,
+        "direction_text": "NB",
+        "terminal": "B",
+        "schedule_relationship": "Scheduled"
+    }
+]
+}
+
+let departureArray = bus_times.departures
+
+// looping over an array to print each departure
+departureArray.forEach(function (departureObject) {
+    let departureTime = departureObject.departure_text
+    let routeID = departureObject.route_id
+    let description = departureObject.description
+    console.log(`${routeID} ${description} ${departureTime}`)
+})
