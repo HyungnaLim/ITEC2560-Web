a5ae5c59342fb018a2233367f56351929181113b
hnlim <email@>
2024-02-27 00:09:50 -0600

    JavaScript callback function example - forEach arrow annotation

--- topic7/callbacks.js
+++ topic7/callbacks.js
@@ -1,5 +1,15 @@
 let animals = ['Giraffe', 'Elephant', 'Yak']
 
+// classic way
 animals.forEach( function(animal, index) { // call back function with index
     console.log(animal, index)
 })
+
+// arrow annotation version
+animals.forEach((animal, index) => {
+    console.log(animal,index)
+})
+
+// shorter version without {}
+// animals.forEach((animal, index) => console.log(animal,index))
+
