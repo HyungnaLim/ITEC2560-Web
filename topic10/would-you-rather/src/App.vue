ad12422ae2cc439058c85b61bc88eb1064f73c38
HyungnaLim <hyungnalim@gmail.com>
2024-03-30 17:24:13 -0500

    Vue + Vite - components, props, emit

---
+++ topic10/would-you-rather/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import WouldYouRather from './components/WouldYouRather.vue'
+
+// import ref to create reactive data
+import {ref} from 'vue'
+
+// create reactive data
+const wyrQuestion = ref('Live in a house shaped like a triangle or a house shaped like a circle')
+const wyrAnswer1 = ref('Triangle house')
+const wyrAnswer2 = ref('Circle house')
+
+// this will store the user's answer once they made a choice
+const userSelection = ref('')
+
+</script>
+
+<template>
+
+<h1>Would You Rather?</h1>
+
+<!-- send the data to WouldYouRather template using v-bind -->
+<WouldYouRather
+  v-bind:question="wyrQuestion"
+  v-bind:answer1="wyrAnswer1"
+  v-bind:answer2="wyrAnswer2">
+</WouldYouRather>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ topic10/would-you-rather/src/components/WouldYouRather.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref } from 'vue'
+
+// props
+// data given to a child (WouldYouRather) by the parent (App)
+// a component should not modify its props
+defineProps({
+  question: String,
+  answer1: String,
+  answer2: String
+})
+
+// reactive state data to store user's answer
+// data belongs to a component, typically used just for this component
+const choice = ref('')
+
+const emit = defineEmits([
+    'answer-selected'
+])
+
+function choiceMade() {
+  // emit event to tell parent that user has made choice
+  emit('answer-selected', choice.value)
+}
+
+</script>
+
+<template>
+  <h2>Make your choice!</h2>
+
+  {{ question }}
+
+  <br>
+
+  <!-- Bind radio buttons using v-model and v-bind -->
+
+  <input v-model="choice" v-bind:value="answer1" v-on:change="choiceMade" type="radio" id="answer1">
+  <label for="answer1">{{ answer1 }}</label>
+
+  <input v-model="choice" v-bind:value="answer2" v-on:change="choiceMade" type="radio" id="answer2">
+  <label for="answer2">{{ answer2 }}</label>
+
+</template>
+
+<style scoped>
+</style>
