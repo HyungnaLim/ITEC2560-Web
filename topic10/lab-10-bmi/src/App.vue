ae4189d26dd62c08a3cc0a77ad07ab0370e8930e
HyungnaLim <hyungnalim@gmail.com>
2024-03-30 18:21:42 -0500

    Lab10 part2

---
+++ topic10/lab-10-bmi/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import BodyMassIndexForm from './components/BodyMassIndexForm.vue'
+
+const bmi = ref('')
+
+const calculateBMI = (height, weight) => {
+  alert('todo - do math!')
+}
+
+</script>
+
+<template>
+  <div>
+
+    <h1>Body Mass Index Calculator</h1>
+
+  </div>
+  <BodyMassIndexForm v-on:stats-entered="calculateBMI"/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ topic10/lab-10-bmi/src/components/BodyMassIndexForm.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+
+})
+
+const height = ref('')
+const weight = ref('')
+
+const emit = defineEmits(['stats-entered'])
+
+function statsEntered() {
+  emit('stats-entered', height.value, weight.value)
+}
+
+</script>
+
+<template>
+
+  <label>Height </label>
+  <input v-model="height">
+
+  <br>
+
+  <label>Weight </label>
+  <input v-model="weight">
+
+  <br>
+
+  <button v-on:click="statsEntered">Calculate</button>
+
+</template>
+
+<style scoped>
+
+button {
+  background-color: skyblue;
+  margin: 10px;
+}
+
+</style>
