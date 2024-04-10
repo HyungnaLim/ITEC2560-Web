c8cc2b5da1c1668f5053a42e41add6ece7455a57
HyungnaLim <hyungnalim@gmail.com>
2024-04-09 21:26:57 -0500

    State Management in Vue apps with Pinia

---
+++ topic11-Vuejs-Pinia/student-sign-in-pinia/src/components/StudentRow.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <tr class="align-middle" v-bind:class="{present:student.present, absent:!student.present}">
+
+    <td> {{student.name}} </td>
+    <td> {{student.starID}} </td>
+
+    <!-- v-model checkbox -->
+    <td> <input type="checkbox" v-on:change="arrivedOrLeft(student)">
+      <span v-if="student.present" class="mx-2">Here!</span>
+      <span v-else class="mx-2">Not present</span>
+    </td>
+
+    <td>
+      <button class="btn btn-danger" v-on:click="deleteStudent(student)">
+        <!-- use bootstrap trashcan icon with i class -->
+        <i class="bi bi-trash-fill"></i> Delete
+      </button>
+    </td>
+
+  </tr>
+
+</template>
+
+<style scoped>
+
+</style>
