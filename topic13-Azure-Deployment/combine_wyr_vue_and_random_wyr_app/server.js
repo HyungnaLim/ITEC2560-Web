7a927da71bc67f954d2f976795087652f9d72b03
HyungnaLim <hyungnalim@gmail.com>
2024-05-01 05:25:24 -0500

    lab13

--- topic13-Azure-Deployment/combine_wyr_vue_and_random_wyr_app/server.js
+++ topic13-Azure-Deployment/combine_wyr_vue_and_random_wyr_app/server.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const path = require('path')
 
 const indexRouter = require('./routes/index')
 
 const app = express()
+
+// find the static data - js, css file created with npx vite build
+const staticFilePath = path.join(__dirname, 'client', 'dist')  // find the directory
+const staticFiles = express.static(staticFilePath)  // get the static files
+app.use('/', staticFiles)  // use the static file
 
 app.use('/', indexRouter)
 
