5e27ca3cccc6fedcd8fcebf5b19e10c6db9114f6
HyungnaLim <hyungnalim@gmail.com>
2024-05-01 05:06:28 -0500

    lab13

--- topic13-Azure-Deployment/combine_wyr_vue_and_random_wyr_app/client/src/services/wyrService.js
+++ topic13-Azure-Deployment/combine_wyr_vue_and_random_wyr_app/client/src/services/wyrService.js
@@ -2,7 +2,7 @@ export default {
     getRandomWYR() {
         return fetch('/wyr').then(response => {  // fetch data from api server and returns a Promise
             // if not indicate server address in full, the program will assume it's the same server - same origin
-            // (ex. '/wyr' will be understood as 'http://localhost:5173/wyr' vue server, not http://localhost:3000/wyr express server)
+            // (ex. '/wyr' will be understood as 'http://localhost:5173/wyr' vite server, not http://localhost:3000/wyr express server)
             // browsers restrict cross-origin requests, it will cause CORS (cross origin request sharing) error
             // so vite dev server need to act as proxy server - configure it in vite.config.js
             return response.json()  // then turns the data into json
